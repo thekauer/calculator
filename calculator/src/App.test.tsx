12ac75d5f97d90efc4b501058db401b1bc8603aa
thekauer <thekauer@gmail.com>
2021-06-15 17:22:49 +0200

    added calculator

--- calculator/src/App.test.tsx
+++ calculator/src/App.test.tsx
@@ -1,9 +1,100 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('test layout', () => {
+  it('tests if layout is rendered properly', () => {
+    const { container } = render(<App />);
+    const grid = container.getElementsByClassName('grid');
+    expect(grid.length).toBe(1);
+    const buttons = container.getElementsByTagName('button');
+    expect(buttons.length).toBe(20);
+    const display = container.getElementsByClassName('display');
+    expect(display.length).toBe(1);
+    const top = container.getElementsByClassName('top');
+    expect(top.length).toBe(1);
+    const bottom = container.getElementsByClassName('bottom');
+    expect(bottom.length).toBe(1);
+  })
+  it('tests if leading zeros are removed', () => {
+    const { container, getByText, getAllByText } = render(<App />);
+    const top = container.getElementsByClassName('top')[0];
+    const bottom = container.getElementsByClassName('bottom')[0];
+    expect(top.innerHTML).toBe("");
+    expect(bottom.innerHTML).toBe("0");
+    const zero = getAllByText(/0/).filter(elem => elem.tagName === 'BUTTON')[0];
+    zero.click();
+    expect(bottom.innerHTML).toBe("0");
+    const one = getByText(/1/);
+    one.click();
+    expect(bottom.innerHTML).toBe("01");
+
+  })
+})
+
+describe('test addition', () => {
+  it('1+1', () => {
+    const { container, getByText } = render(<App />);
+    const top = container.getElementsByClassName('top')[0];
+    const bottom = container.getElementsByClassName('bottom')[0];
+    const one = getByText(/1/);
+    const add = getByText(/^\+$/);
+    const eq = getByText(/=/);
+    one.click();
+    expect(bottom.innerHTML).toBe('1');
+    add.click();
+    expect(bottom.innerHTML).toBe('0');
+    expect(top.innerHTML).toBe('1+');
+    one.click();
+    expect(top.innerHTML).toBe('1+');
+    expect(bottom.innerHTML).toBe('01');
+    eq.click();
+    expect(top.innerHTML).toBe(" ");
+    expect(bottom.innerHTML).toBe('2');
+  })
+  it('1+0', () => {
+    const { container, getByText, getAllByText } = render(<App />);
+    const top = container.getElementsByClassName('top')[0];
+    const bottom = container.getElementsByClassName('bottom')[0];
+    const one = getByText(/1/);
+    const add = getByText(/^\+$/);
+    const eq = getByText(/=/);
+    one.click();
+    expect(bottom.innerHTML).toBe('1');
+    add.click();
+    expect(bottom.innerHTML).toBe('0');
+    expect(top.innerHTML).toBe('1+');
+    eq.click();
+    expect(bottom.innerHTML).toBe('1');
+  })
+
+})
+
+describe('substraction', () => {
+  it('1-2-1', () => {
+    const { container, getByText } = render(<App />);
+    const top = container.getElementsByClassName('top')[0];
+    const bottom = container.getElementsByClassName('bottom')[0];
+    const one = getByText(/1/);
+    const sub = getByText(/^-$/);
+    const two = getByText(/2/);
+    const eq = getByText(/=/);
+    one.click();
+    expect(bottom.innerHTML).toBe('1');
+    sub.click();
+    expect(bottom.innerHTML).toBe('0');
+    expect(top.innerHTML).toBe('1-');
+    two.click();
+    expect(top.innerHTML).toBe('1-');
+    expect(bottom.innerHTML).toBe('02');
+    eq.click();
+    expect(top.innerHTML).toBe(" ");
+    expect(bottom.innerHTML).toBe('-1');
+    sub.click();
+    one.click();
+    eq.click();
+    expect(top.innerHTML).toBe(" ");
+    expect(bottom.innerHTML).toBe('-2');
+
+  })
+})
+
--- calculator/src/App.tsx
+++ calculator/src/App.tsx
@@ -1,25 +1,154 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
+interface Equation {
+  lhs?:number;
+  op?:string
+}
+
 function App() {
+  const [history,setHistory] = useState("");
+  const [display,setDisplay] = useState("0");
+  const [equation,setEquation] = useState<Equation>({});
+  const [override,setOverride] = useState(true);
+  const peekMemory = () => {
+
+  }
+  const storeInMemory = () => {
+
+  }
+  const clearDisplay = () => {
+    if(display==="0") {
+      setHistory(" ");
+    } else {
+      setDisplay("0");
+    }
+  }
+  const evalDisplay = () => {
+    if(override) return;
+    const rhs = Number.parseFloat(display);
+    let result = 0;
+    if(equation.op && equation.lhs) {
+      setHistory(" ");
+      switch(equation.op) {
+        case '+':
+          result = equation.lhs + rhs;
+          break;
+        case '-':
+          result = equation.lhs - rhs;
+          break;
+        case '×':
+          result = equation.lhs * rhs;
+          break;
+        case '÷':
+          if(rhs!==0) {
+            result = equation.lhs / rhs;
+          } else {
+            setDisplay("Nem lehet 0-val osztani");
+            setOverride(true);
+            setEquation({});
+            return;
+          }
+          break;
+      }
+      return result;
+    }
+  }
+  const addToDisplay = (s: string) => {
+    return () => {
+      if(override) {
+        setDisplay(s);
+        setOverride(false);
+      } else {
+        setDisplay(display + s);
+      }
+    }
+  }
+  const operatorClick = (op : string) => {
+    return () => {
+      if(!override) {
+        if(!equation.lhs) { 
+          const lhs = Number.parseFloat(display);
+          setEquation({lhs,op});
+          setHistory(lhs+op)
+          setDisplay("0");
+        } else {
+          const old = display;
+          const lhs = evalDisplay();
+          setEquation({lhs,op});
+          setHistory(lhs+op);
+          setDisplay(old);
+
+        }
+      } 
+    }
+  }
+
+  const equalsClick = () => {
+    const result = evalDisplay();
+    if(result) {
+      setDisplay(result.toString());
+      setEquation({});
+    }
+  }
+
+  const buttons = [
+    [
+      { text: "M-", fn: peekMemory },
+      { text: "M+", fn: storeInMemory },
+      { text: "C", fn: clearDisplay },
+      { text: "÷", fn: operatorClick("÷") }
+    ],
+    [
+      { text: "7", fn: addToDisplay("7") },
+      { text: "8", fn: addToDisplay("8") },
+      { text: "9", fn: addToDisplay("9") },
+      { text: "×", fn: operatorClick("×") }
+    ],
+    [
+      { text: "4", fn: addToDisplay("4") },
+      { text: "5", fn: addToDisplay("5") },
+      { text: "6", fn: addToDisplay("6") },
+      { text: "-", fn: operatorClick("-") }
+    ],
+    [
+      { text: "1", fn: addToDisplay("1") },
+      { text: "2", fn: addToDisplay("2") },
+      { text: "3", fn: addToDisplay("3") },
+      { text: "+", fn: operatorClick("+") }
+    ],
+    [
+      { text: "±", fn: () => {} },
+      { text: "0", fn: addToDisplay("0") },
+      { text: ",", fn: addToDisplay(",") },
+      { text: "=", fn: equalsClick }
+    ]
+  ]
+
+  useEffect(()=> {
+    if(display.startsWith("0") && display.length > 1) {
+      setDisplay(display.substr(1));
+    }
+  }, [display])
+
+  const drawButtons = () => {
+    return buttons.map((arr, row) => arr.map((button, col) => {
+      return (
+        <button key={row*4+col} style={{ gridRow: row + 1 + 2, gridColumn: col + 1 }} onClick={button.fn}>{button.text}</button>
+      )
+    }))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="grid">
+        <div className="display">
+          <div className="top">{history}</div>
+          <div className="bottom">{display}</div>
+        </div>
+        {drawButtons()}
+      </div>
+    </>
   );
 }
 
