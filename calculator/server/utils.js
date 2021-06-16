4c5c85a82e3e1647677cb5846dec8349ba344122
thekauer <thekauer@gmail.com>
2021-06-16 11:42:29 +0200

    added docs

--- calculator/server/utils.js
+++ calculator/server/utils.js
@@ -3,13 +3,22 @@ const readline = require('readline');
 
 const dbFile = 'db.txt';
 let users = new Set();
-
+/**
+ * @returns a random string of letters and numbers with a length of 8
+ */
 const getRandomString = () => Math.random().toString(36).substr(2, 10);
-
+/**
+ * Store a number for a user
+ * @param {string} id id of the user
+ * @param {number} number number to be saved
+ */
 const saveNumber = (id,number) => {
     users.add(id);
     fs.appendFileSync(dbFile,id + " " +number.toString() + "\n");
 }
+/**
+ * @returns a unique identifier for a user
+ */
 const generateId = () => {
     let id;
     do {
@@ -18,7 +27,11 @@ const generateId = () => {
     while(users.has(id));
     return id;
 }
-
+/**
+ * 
+ * @param {string} id id of the user 
+ * @returns the number of the user
+ */
 const getNumberForId = (id) => {
     const fileStream = fs.createReadStream(dbFile);
 
@@ -35,11 +48,17 @@ const getNumberForId = (id) => {
     }
     return get();
 }
-
+/**
+ * Checks if user exists in database
+ * @param {string} id id of the user
+ * @returns true if the database includes the user
+ */
 const isUser = (id) => {
     return users.has(id);
 }
-
+/**
+ * Initialize server by deleting the old database file
+ */
 const initServer = () => {
     console.log('running on port 8080');
     fs.unlinkSync(dbFile);
