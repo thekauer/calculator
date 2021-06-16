59ff299049e6e0a47b8e70b9d46448554e970eaa
thekauer <thekauer@gmail.com>
2021-06-16 09:26:40 +0200

    fixed server

--- calculator/server/index.js
+++ calculator/server/index.js
@@ -1,5 +1,5 @@
 "use strict";
-const {generateId,getNumberForId, saveNumber,isUser} = require('./utils');
+const {generateId,getNumberForId, saveNumber,isUser,initServer} = require('./utils');
 const express = require("express");
 const bodyParser = require('body-parser');
 const app = express();
@@ -10,17 +10,17 @@ app.use(bodyParser.text());
 
 
 app.post('/api/store', (req, res) => {
-    const id = generateId();
-    console.log(id);   
+    const id = generateId(); 
     const number = req.body;
     saveNumber(id,number);
     res.send(id);
 });
 
-app.post('/api/pop', (req, res) => {
+app.post('/api/pop', async (req, res) => {
     const id = req.body;
     if(!isUser(id)) res.status(400).send();
-    const number = getNumberForId(id);
+    const number = await getNumberForId(id);
+    console.log(number);
     if(number) {
         res.status(200).send(number.toString());
     } else {
@@ -28,4 +28,4 @@ app.post('/api/pop', (req, res) => {
     }
 });
 
-app.listen(8080,()=>{console.log('running on port 8080')});
+app.listen(8080,initServer);
--- calculator/server/utils.js
+++ calculator/server/utils.js
@@ -7,6 +7,7 @@ let users = new Set();
 const getRandomString = () => Math.random().toString(36).substr(2, 10);
 
 const saveNumber = (id,number) => {
+    users.add(id);
     fs.appendFileSync(dbFile,id + " " +number.toString() + "\n");
 }
 const generateId = () => {
@@ -24,23 +25,30 @@ const getNumberForId = (id) => {
     const rl = readline.createInterface({
         input: fileStream,
     });
-
-    for (const line of rl) { //FIX error rl not iterable
-        const [first, second, ...rest] = line.split(' ');
-        if (first === id) {
-            return second;
+    const get = async () => {
+        for await (const line of rl) {
+            const [first, second, ...rest] = line.split(' ');
+            if (first === id) {
+                return second;
+            }
         }
     }
+    return get();
 }
 
 const isUser = (id) => {
     return users.has(id);
 }
 
+const initServer = () => {
+    console.log('running on port 8080');
+    fs.unlinkSync(dbFile);
+}
 
 module.exports= {
     generateId,
     getNumberForId,
     saveNumber,
-    isUser
+    isUser,
+    initServer
 }
